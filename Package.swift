dba430c2a524036f1ae6ecf8ebd4ec2f0edfecae
Lyuboslav Ivanov <luboslav.ivanov@azdigital.bg>
2023-01-25 15:52:25 +0200

    [UPDATE]: Package dependancies

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         ],
         dependencies: [
             .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-            .package(url: "https://github.com/horizontalsystems/ThemeKit.Swift.git", .upToNextMajor(from: "1.0.0")),
-            .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/LuboDev/ThemeKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/LuboDev/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
         ],
         targets: [
             .target(
